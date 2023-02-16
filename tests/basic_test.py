48c7ac4036256a27f8d154ea573be612e3931c0b
Jorge Garcia de Bustos <j.garciadebustos@godeltech.com>
2023-02-16 11:04:21 +0000

    fix smells suggested by SonarCloud

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -22,18 +22,28 @@ HEADERS = {
     "Content-Type": "application/json",
     "Accept": "application/json",
 }
+STRING_PARAM = "string_param"
+INT_PARAM = "int_param"
+FLOAT_PARAM = "float_param"
+BOOL_PARAM = "bool_param"
+DATETIME_PARAM = "datetime_param"
+DATE_PARAM = "date_param"
 GOOD_JSON_DICT = {
-    "string_param": "foobar",
-    "int_param": 12345,
-    "float_param": 123.45,
-    "bool_param": True,
-    "datetime_param": "2021-11-30T14:37:04.150Z",
-    "date_param": "2021-10-26",
+    STRING_PARAM: "foobar",
+    INT_PARAM: 12345,
+    FLOAT_PARAM: 123.45,
+    BOOL_PARAM: True,
+    DATETIME_PARAM: "2021-11-30T14:37:04.150Z",
+    DATE_PARAM: "2021-10-26",
 }
 REQUEST_TIMEOUT = 0.1  # 100 milliseconds, life is too short
 
+MOCK_PREDICTION = "mock_prediction"
 NOT_A_DATETIME_MSG = "is not a 'date-time'"
 NOT_A_DATE_MSG = "is not a 'date'"
+VALUE_ERROR_MISSING = "value_error.missing"
+VALUE_ERROR_DATE = "value_error.date"
+VALUE_ERROR_DATETIME = "value_error.datetime"
 
 
 def _get_request(url):
@@ -133,267 +143,267 @@ def test_post_model_predict_empty_json_status_code_equals_422():
     properties as required."""
     response = _post_request(ROOT_URL + MODEL_PREDICT_PATH, headers=HEADERS, json={})
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.missing" in response.text
-    assert "string_param" in response.text
-    assert "int_param" in response.text
-    assert "float_param" in response.text
-    assert "bool_param" in response.text
-    assert "datetime_param" in response.text
-    assert "date_param" in response.text
+    assert VALUE_ERROR_MISSING in response.text
+    assert STRING_PARAM in response.text
+    assert INT_PARAM in response.text
+    assert FLOAT_PARAM in response.text
+    assert BOOL_PARAM in response.text
+    assert DATETIME_PARAM in response.text
+    assert DATE_PARAM in response.text
 
 
 def test_post_model_predict_good_json_status_code_equals_200():
     """Verify that calling /model/predict with good payload succeeds with 422 and returns
-    "mock_prediction"."""
+    MOCK_PREDICTION."""
     response = _post_request_good_json_with_overrides()
     assert response.status_code == HTTPStatus.OK
-    assert "mock_prediction" in response.text
+    assert MOCK_PREDICTION in response.text
 
 
 def test_post_model_predict_datetime_param_missing_z_status_code_equals_200():
     """Verify that calling /model/predict with bad datetime_param (missing ending Z) works."""
     response = _post_request_good_json_with_overrides(
-        "datetime_param", "2021-11-30T14:37:04.15"
+        DATETIME_PARAM, "2021-11-30T14:37:04.15"
     )
     assert response.status_code == HTTPStatus.OK
-    assert "mock_prediction" in response.text
+    assert MOCK_PREDICTION in response.text
 
 
 def test_post_model_predict_string_param_float_status_code_equals_200():
     """Verify that calling /model/predict with float as string_param works fine
     (yes, that's now pydantic rolls)."""
-    response = _post_request_good_json_with_overrides("string_param", 10.1)
+    response = _post_request_good_json_with_overrides(STRING_PARAM, 10.1)
     assert response.status_code == HTTPStatus.OK
-    assert "mock_prediction" in response.text
+    assert MOCK_PREDICTION in response.text
 
 
 def test_post_model_predict_int_param_float_status_code_equals_200():
     """Verify that calling /model/predict with float as int_param works fine
     (yes, that's now pydantic rolls)."""
-    response = _post_request_good_json_with_overrides("int_param", 10.1)
+    response = _post_request_good_json_with_overrides(INT_PARAM, 10.1)
     assert response.status_code == HTTPStatus.OK
-    assert "mock_prediction" in response.text
+    assert MOCK_PREDICTION in response.text
 
 
 def test_post_model_predict_bool_param_string_true_status_code_equals_200():
     """Verify that calling /model/predict with string "2"True" as bool_param works fine
     (yes, that's now pydantic rolls)."""
-    response = _post_request_good_json_with_overrides("bool_param", "True")
+    response = _post_request_good_json_with_overrides(BOOL_PARAM, "True")
     assert response.status_code == HTTPStatus.OK
-    assert "mock_prediction" in response.text
+    assert MOCK_PREDICTION in response.text
 
 
 def test_post_model_predict_int_param_missing_status_code_equals_422():
     """Verify that calling /model/predict with no int_param fails with 422 and flags missing
     properties as required."""
-    response = _post_request_good_json_with_overrides("int_param", None)
+    response = _post_request_good_json_with_overrides(INT_PARAM, None)
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.missing" in response.text
-    assert "int_param" in response.text
+    assert VALUE_ERROR_MISSING in response.text
+    assert INT_PARAM in response.text
 
 
 def test_post_model_predict_string_param_missing_status_code_equals_422():
     """Verify that calling /model/predict with no string_param fails with 422 and flags missing
     properties as required."""
-    response = _post_request_good_json_with_overrides("string_param", None)
+    response = _post_request_good_json_with_overrides(STRING_PARAM, None)
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.missing" in response.text
-    assert "string_param" in response.text
+    assert VALUE_ERROR_MISSING in response.text
+    assert STRING_PARAM in response.text
 
 
 def test_post_model_predict_float_param_missing_status_code_equals_422():
     """Verify that calling /model/predict with no float_param fails with 422 and flags missing
     properties as required."""
-    response = _post_request_good_json_with_overrides("float_param", None)
+    response = _post_request_good_json_with_overrides(FLOAT_PARAM, None)
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.missing" in response.text
-    assert "float_param" in response.text
+    assert VALUE_ERROR_MISSING in response.text
+    assert FLOAT_PARAM in response.text
 
 
 def test_post_model_predict_bool_param_missing_status_code_equals_422():
     """Verify that calling /model/predict with no bool_param fails with 422 and flags missing
     properties as required."""
-    response = _post_request_good_json_with_overrides("bool_param", None)
+    response = _post_request_good_json_with_overrides(BOOL_PARAM, None)
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.missing" in response.text
-    assert "bool_param" in response.text
+    assert VALUE_ERROR_MISSING in response.text
+    assert BOOL_PARAM in response.text
 
 
 def test_post_model_predict_datetime_param_missing_status_code_equals_422():
     """Verify that calling /model/predict with no datetime_param fails with 422 and flags
     missing properties as required."""
-    response = _post_request_good_json_with_overrides("datetime_param", None)
+    response = _post_request_good_json_with_overrides(DATETIME_PARAM, None)
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.missing" in response.text
-    assert "datetime_param" in response.text
+    assert VALUE_ERROR_MISSING in response.text
+    assert DATETIME_PARAM in response.text
 
 
 def test_post_model_predict_date_param_missing_status_code_equals_422():
     """Verify that calling /model/predict with no date_param fails with 422 and flags missing
     properties as required."""
-    response = _post_request_good_json_with_overrides("date_param", None)
+    response = _post_request_good_json_with_overrides(DATE_PARAM, None)
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.missing" in response.text
-    assert "date_param" in response.text
+    assert VALUE_ERROR_MISSING in response.text
+    assert DATE_PARAM in response.text
 
 
 def test_post_model_predict_int_param_string_status_code_equals_422():
     """Verify that calling /model/predict with string as int_param fails with 422 and flags
     wrong type."""
-    response = _post_request_good_json_with_overrides("int_param", "a")
+    response = _post_request_good_json_with_overrides(INT_PARAM, "a")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
     assert "type_error.integer" in response.text
-    assert "int_param" in response.text
+    assert INT_PARAM in response.text
 
 
 def test_post_model_predict_float_param_string_status_code_equals_422():
     """Verify that calling /model/predict with string as float_param fails with 422 and flags
     wrong type."""
-    response = _post_request_good_json_with_overrides("float_param", "a")
+    response = _post_request_good_json_with_overrides(FLOAT_PARAM, "a")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
     assert "type_error.float" in response.text
-    assert "float_param" in response.text
+    assert FLOAT_PARAM in response.text
 
 
 def test_post_model_predict_bool_param_string_status_code_equals_422():
     """Verify that calling /model/predict with string as bool_param fails with 422 and flags
     wrong type."""
-    response = _post_request_good_json_with_overrides("bool_param", "foo")
+    response = _post_request_good_json_with_overrides(BOOL_PARAM, "foo")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
     assert "type_error.bool" in response.text
-    assert "bool_param" in response.text
+    assert BOOL_PARAM in response.text
 
 
 def test_post_model_predict_datetime_param_bad_day_status_code_equals_422():
     """Verify that calling /model/predict with bad datetime_param (impossible day number) fails
     with 422 and flags wrong type."""
     response = _post_request_good_json_with_overrides(
-        "datetime_param", "2021-11-35T14:37:04.15Z"
+        DATETIME_PARAM, "2021-11-35T14:37:04.15Z"
     )
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.datetime" in response.text
-    assert "datetime_param" in response.text
+    assert VALUE_ERROR_DATETIME in response.text
+    assert DATETIME_PARAM in response.text
 
 
 def test_post_model_predict_datetime_param_bad_month_status_code_equals_422():
     """Verify that calling /model/predict with bad datetime_param (impossible month number)
     fails with 422 and flags wrong type."""
     response = _post_request_good_json_with_overrides(
-        "datetime_param", "2021-13-30T14:37:04.15Z"
+        DATETIME_PARAM, "2021-13-30T14:37:04.15Z"
     )
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.datetime" in response.text
-    assert "datetime_param" in response.text
+    assert VALUE_ERROR_DATETIME in response.text
+    assert DATETIME_PARAM in response.text
 
 
 def test_post_model_predict_datetime_param_bad_year_status_code_equals_422():
     """Verify that calling /model/predict with bad datetime_param (impossible year number) fails
     with 422 and flags wrong type."""
     response = _post_request_good_json_with_overrides(
-        "datetime_param", "999-11-30T14:37:04.15Z"
+        DATETIME_PARAM, "999-11-30T14:37:04.15Z"
     )
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.datetime" in response.text
-    assert "datetime_param" in response.text
+    assert VALUE_ERROR_DATETIME in response.text
+    assert DATETIME_PARAM in response.text
 
 
 def test_post_model_predict_datetime_param_missing_hyphen_status_code_equals_422():
     """Verify that calling /model/predict with bad datetime_param (missing date hyphen) fails
     with 422 and flags wrong type."""
     response = _post_request_good_json_with_overrides(
-        "datetime_param", "2021-1130T14:37:04.15Z"
+        DATETIME_PARAM, "2021-1130T14:37:04.15Z"
     )
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.datetime" in response.text
-    assert "datetime_param" in response.text
+    assert VALUE_ERROR_DATETIME in response.text
+    assert DATETIME_PARAM in response.text
 
 
 def test_post_model_predict_datetime_param_missing_t_status_code_equals_422():
     """Verify that calling /model/predict with bad datetime_param (missing T between date and
     time) fails with 422 and flags wrong type."""
     response = _post_request_good_json_with_overrides(
-        "datetime_param", "2021-11-3014:37:04.15Z"
+        DATETIME_PARAM, "2021-11-3014:37:04.15Z"
     )
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.datetime" in response.text
-    assert "datetime_param" in response.text
+    assert VALUE_ERROR_DATETIME in response.text
+    assert DATETIME_PARAM in response.text
 
 
 def test_post_model_predict_datetime_param_missing_colon_status_code_equals_422():
     """Verify that calling /model/predict with bad datetime_param (missing time colon) fails
     with 422 and flags wrong type."""
     response = _post_request_good_json_with_overrides(
-        "datetime_param", "2021-11-30T1437:04.15Z"
+        DATETIME_PARAM, "2021-11-30T1437:04.15Z"
     )
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.datetime" in response.text
-    assert "datetime_param" in response.text
+    assert VALUE_ERROR_DATETIME in response.text
+    assert DATETIME_PARAM in response.text
 
 
 def test_post_model_predict_datetime_param_bad_hour_status_code_equals_422():
     """Verify that calling /model/predict with bad datetime_param (impossible hour number)
     fails with 422 and flags wrong type."""
     response = _post_request_good_json_with_overrides(
-        "datetime_param", "2021-11-30T34:37:04.15Z"
+        DATETIME_PARAM, "2021-11-30T34:37:04.15Z"
     )
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.datetime" in response.text
-    assert "datetime_param" in response.text
+    assert VALUE_ERROR_DATETIME in response.text
+    assert DATETIME_PARAM in response.text
 
 
 def test_post_model_predict_datetime_param_bad_minute_status_code_equals_422():
     """Verify that calling /model/predict with bad datetime_param (impossible minute number)
     fails with 422 and flags wrong type."""
     response = _post_request_good_json_with_overrides(
-        "datetime_param", "2021-11-30T14:77:04.15Z"
+        DATETIME_PARAM, "2021-11-30T14:77:04.15Z"
     )
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.datetime" in response.text
-    assert "datetime_param" in response.text
+    assert VALUE_ERROR_DATETIME in response.text
+    assert DATETIME_PARAM in response.text
 
 
 def test_post_model_predict_datetime_param_bad_second_status_code_equals_422():
     """Verify that calling /model/predict with bad datetime_param (impossible second number)
     fails with 422 and flags wrong type."""
     response = _post_request_good_json_with_overrides(
-        "datetime_param", "2021-11-30T14:37:64.15Z"
+        DATETIME_PARAM, "2021-11-30T14:37:64.15Z"
     )
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.datetime" in response.text
-    assert "datetime_param" in response.text
+    assert VALUE_ERROR_DATETIME in response.text
+    assert DATETIME_PARAM in response.text
 
 
 def test_post_model_predict_date_param_bad_year_status_code_equals_422():
     """Verify that calling /model/predict with bad date_param (impossible year number) fails
     with 422 and flags wrong type."""
-    response = _post_request_good_json_with_overrides("date_param", "999-10-26")
+    response = _post_request_good_json_with_overrides(DATE_PARAM, "999-10-26")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.date" in response.text
-    assert "date_param" in response.text
+    assert VALUE_ERROR_DATE in response.text
+    assert DATE_PARAM in response.text
 
 
 def test_post_model_predict_date_param_bad_month_status_code_equals_422():
     """Verify that calling /model/predict with bad date_param (impossible month number) fails
     with 422 and flags wrong type."""
-    response = _post_request_good_json_with_overrides("date_param", "2021-18-26")
+    response = _post_request_good_json_with_overrides(DATE_PARAM, "2021-18-26")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.date" in response.text
-    assert "date_param" in response.text
+    assert VALUE_ERROR_DATE in response.text
+    assert DATE_PARAM in response.text
 
 
 def test_post_model_predict_date_param_bad_day_status_code_equals_422():
     """Verify that calling /model/predict with bad date_param (impossible day number) fails
     with 422 and flags wrong type."""
-    response = _post_request_good_json_with_overrides("date_param", "2021-10-51")
+    response = _post_request_good_json_with_overrides(DATE_PARAM, "2021-10-51")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.date" in response.text
-    assert "date_param" in response.text
+    assert VALUE_ERROR_DATE in response.text
+    assert DATE_PARAM in response.text
 
 
 def test_post_model_predict_date_param_missing_hyphen_status_code_equals_422():
     """Verify that calling /model/predict with bad date_param (missing date hyphen) fails with
     422 and flags wrong type."""
-    response = _post_request_good_json_with_overrides("date_param", "2021-1026")
+    response = _post_request_good_json_with_overrides(DATE_PARAM, "2021-1026")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-    assert "value_error.date" in response.text
-    assert "date_param" in response.text
+    assert VALUE_ERROR_DATE in response.text
+    assert DATE_PARAM in response.text
